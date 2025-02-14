3f170f6e3102b71edfde6ac7fc76c3ca85ea7a50
matthughesr <82174638+matthughesr@users.noreply.github.com>
2025-02-14 04:42:34 +0000

    Gets each page

--- script.sh
+++ script.sh
@@ -5,38 +5,52 @@
 #Notes: -r helps with errors. Doesn't treat backslash as escape characters
 #Notes: curl -s is the silent version meaning no progress outputed 
 #Notes: https://eliatra.com/blog/json-processing-command-line-jq/
-echo""
-echo"-------------Starting Star Wars Script-------------"
+echo ""
+echo "-------------Starting Star Wars Script-------------"
 
 mainURL="https://swapi.dev/api/starships/"
 
-# Get the list of starships; includes all starship info. -O will send it to standardoutput
-response=$(curl -s "$mainURL") 
+ 
+count=0;
 
-#gets echo starship from the json file. 
-starships=$(echo "$response" | jq -c '.results[]')
 
-# Loop through each starship and get name 
-echo "$starships" | while read -r oneShip; 
-    do
-    name=$(echo "$oneShip" | jq -r '.name') #get name of ship
-    echo "Starship: $name"  #print out the name of ship
-    
-    # Get pilot info
-    pilots=$(echo "$oneShip" | jq -r '.pilots | .[]?')  # ?handles errors if array is empty
-    if [ -z "$pilots" ]; then
-        echo "Pilots: None" #if there is not pilot print none
-    else
-        echo "Pilots:"
-        echo "$pilots" | while read -r pilot_url; # read in pilot URL until its empty
-            do
-            pilot_response=$(curl -s "$pilot_url")
-            pilot_name=$(echo "$pilot_response" | jq -r '.name')
-            echo "  - $pilot_name"
+while [[ -n $mainURL ]];
+do
+
+    # Get the list of starships; includes all starship info. 
+    response=$(curl -s "$mainURL")
+
+    #gets starship from the json file. 
+    starships=$(echo "$response" | jq -c '.results[]')
+
+    # Loop through each starship and get name 
+    echo "$starships" | while read -r oneShip; 
+        do
+        name=$(echo "$oneShip" | jq -r '.name') #get name of ship
+        echo "Starship: $name"  #print out the name of ship
+      
+        
+        # Get pilot info
+        pilots=$(echo "$oneShip" | jq -r '.pilots | .[]?')  # ?handles errors if array is empty
+        if [ -z "$pilots" ]; then
+            echo "Pilots: None" #if there is not pilot print none
+        else
+            echo "Pilots:"
+            echo "$pilots" | while read -r pilot_url; # read in pilot URL until its empty
+                do
+                pilot_response=$(curl -s "$pilot_url")
+                pilot_name=$(echo "$pilot_response" | jq -r '.name')
+                echo "  - $pilot_name"
+            done
+        fi
+        echo "----------------------------------------"
+         
         done
-    fi
-    echo "----------------------------------------"
+        
+    mainURL=$(echo "$response" | jq -r '.next')
+    [[ "$mainURL" == "null" ]] && mainURL=""
+   
 done
-
+#echo "There are $count starships"
 echo "Star Wars Script is done. Bye"
 exit 0
